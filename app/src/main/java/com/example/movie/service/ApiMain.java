3d1bfe0d3ff3031730df1b4a54b8bacda5159b22
octcoo <oktaviaisnania@gmail.com>
2020-04-23 15:35:40 +0700

    Movie on process

--- app/src/main/java/com/example/movie/service/ApiMain.java
+++ app/src/main/java/com/example/movie/service/ApiMain.java
@@ -7,7 +7,7 @@ public class ApiMain {
     private Retrofit retrofit;
 
     public MovieRepository getApiMovie() {
-        String BASE_URL = "https://api.themoviedb.org/3";
+        String BASE_URL = "https://api.themoviedb.org/";
         if (retrofit == null) {
             retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         }
--- app/src/main/java/com/example/movie/service/MovieRepository.java
+++ app/src/main/java/com/example/movie/service/MovieRepository.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface MovieRepository {
-    @GET("discover/movie?api_key=05faacecb1bb8a123ad56542b1708bad")
+    @GET("3/discover/movie?api_key=05faacecb1bb8a123ad56542b1708bad")
     Call<MovieResponse> getMovie();
 }
